4d97ced9841a21a8a34312a71b6301cfb918495f
Adam Coddington <adam@parthenonsoftware.com>
2011-11-22 11:12:00 -0800

    Switching to south migration management; refs #9561.

---
+++ countries/migrations/0001_initial.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding model 'Country'
+        db.create_table('countries_country', (
+            ('iso', self.gf('django.db.models.fields.CharField')(max_length=2, primary_key=True)),
+            ('name', self.gf('django.db.models.fields.CharField')(max_length=128)),
+            ('printable_name', self.gf('django.db.models.fields.CharField')(max_length=128)),
+            ('iso3', self.gf('django.db.models.fields.CharField')(max_length=3, null=True)),
+            ('numcode', self.gf('django.db.models.fields.PositiveSmallIntegerField')(null=True)),
+        ))
+        db.send_create_signal('countries', ['Country'])
+
+    def backwards(self, orm):
+        # Deleting model 'Country'
+        db.delete_table('countries_country')
+
+    models = {
+        'countries.country': {
+            'Meta': {'ordering': "('name',)", 'object_name': 'Country'},
+            'iso': ('django.db.models.fields.CharField', [], {'max_length': '2', 'primary_key': 'True'}),
+            'iso3': ('django.db.models.fields.CharField', [], {'max_length': '3', 'null': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'numcode': ('django.db.models.fields.PositiveSmallIntegerField', [], {'null': 'True'}),
+            'printable_name': ('django.db.models.fields.CharField', [], {'max_length': '128'})
+        }
+    }
+
+    complete_apps = ['countries']
